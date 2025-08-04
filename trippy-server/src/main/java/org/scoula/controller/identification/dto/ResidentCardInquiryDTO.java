dac22f9845d77de9f417876dde5bcbd89d3028ea
bangbang444 <kim1228com@naver.com>
2025-08-04 14:04:45 +0900

    feat: OCR 결과 DTO 작성

---
+++ trippy-server/src/main/java/org/scoula/controller/identification/dto/ResidentCardInquiryDTO.java
@@ -0,0 +1,19 @@
+package org.scoula.controller.identification.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+
+@ApiModel(description = "신분증 DTO")
+public record ResidentCardInquiryDTO(
+        @ApiModelProperty(value = "사용자 이름")
+        String resUserName,
+        @ApiModelProperty(value = "발행 날짜")
+        String resIssueDate,
+        @ApiModelProperty(value = "주민 등록 번호")
+        String resUserIdentity,
+        @ApiModelProperty(value = "거주지 주소")
+        String address,
+        @ApiModelProperty(value = "신분증 사진")
+        String photo
+) {}
