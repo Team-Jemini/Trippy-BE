406f0556c571c9c637680509b58e66d7e62efb17
bangbang444 <kim1228com@naver.com>
2025-08-04 15:20:56 +0900

    feat: 서버 에러 예외 추가

--- trippy-server/src/main/java/org/scoula/common/exception/model/ServerErrorException.java
+++ trippy-server/src/main/java/org/scoula/common/exception/model/ServerErrorException.java
@@ -1,9 +1,11 @@
 package org.scoula.common.exception.model;
 
+import lombok.Getter;
 import org.scoula.common.exception.enums.ErrorCode;
 
+
 public class ServerErrorException extends TrippyException {
-    public ServerErrorException(ErrorCode errorCode) {
-        super(errorCode);
-    }
+  public ServerErrorException(ErrorCode errorCode) {
+    super(errorCode);
+  }
 }
