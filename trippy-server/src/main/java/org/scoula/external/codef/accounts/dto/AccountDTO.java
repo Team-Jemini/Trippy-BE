7b445db8bd1cab617171ea5189e0bced424bb7d4
i-soj-ng <jeong4530@gmail.com>
2025-08-04 15:13:17 +0900

    feat : 개인 계좌 DTO 작성

---
+++ trippy-server/src/main/java/org/scoula/external/codef/accounts/dto/AccountDTO.java
@@ -0,0 +1,10 @@
+package org.scoula.external.codef.accounts.dto;
+
+public class AccountDTO {
+    private String resAccount; // 계좌번호
+    private String resAccountBalance; // 잔액
+    private String resAccountCurrency; // 통화 코드
+    private String resAccountName; // 계좌명
+    private String resAccountDisplay; // 표시용 계좌번호
+    private String resAccountEndDate; // 만기일
+}
---
+++ trippy-server/src/main/java/org/scoula/external/codef/accounts/service/AccountService.java
@@ -0,0 +1,4 @@
+package org.scoula.external.codef.accounts.service;
+
+public class AccountService {
+}
