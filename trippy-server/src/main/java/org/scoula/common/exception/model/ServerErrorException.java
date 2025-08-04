d85d73768959741faaceb23db1433b9d08997afa
bangbang444 <kim1228com@naver.com>
2025-08-04 17:29:27 +0900

    chore: 불필요한 import 제거

--- trippy-server/src/main/java/org/scoula/common/exception/model/ServerErrorException.java
+++ trippy-server/src/main/java/org/scoula/common/exception/model/ServerErrorException.java
@@ -1,8 +1,6 @@
 package org.scoula.common.exception.model;
 
-import lombok.Getter;
 import org.scoula.common.exception.enums.ErrorCode;
-
 
 public class ServerErrorException extends TrippyException {
   public ServerErrorException(ErrorCode errorCode) {
