68d76f0bb5b6053ef99106f904c0f05d6abef01e
hellozo0 <ahju0011@gmail.com>
2025-08-04 11:19:36 +0900

    fix : Rootconfig ComponentScan 경로 확장

--- trippy-server/src/main/java/org/scoula/config/RootConfig.java
+++ trippy-server/src/main/java/org/scoula/config/RootConfig.java
@@ -1,7 +1,7 @@
 package org.scoula.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
@@ -14,51 +14,52 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
-import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @PropertySource({"classpath:/application.properties"})
 @MapperScan("org.scoula.mapper")
-@ComponentScan(basePackages = {"org.scoula.config"})
+@ComponentScan(basePackages = {"org.scoula"})
 public class RootConfig {
-    @Value("${jdbc.driver}")
-    String driver;
-    @Value("${jdbc.url}")
-    String url;
-    @Value("${jdbc.username}")
-    String username;
-    @Value("${jdbc.password}")
-    String password;
-    @Autowired
-    ApplicationContext applicationContext;
+	@Value("${jdbc.driver}")
+	String driver;
+	@Value("${jdbc.url}")
+	String url;
+	@Value("${jdbc.username}")
+	String username;
+	@Value("${jdbc.password}")
+	String password;
+	@Autowired
+	ApplicationContext applicationContext;
 
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
 
-        config.setDriverClassName(driver);
-        config.setJdbcUrl(url);
-        config.setUsername(username);
-        config.setPassword(password);
+		config.setDriverClassName(driver);
+		config.setJdbcUrl(url);
+		config.setUsername(username);
+		config.setPassword(password);
 
-        HikariDataSource dataSource = new HikariDataSource(config);
-        return dataSource;
-    }
+		HikariDataSource dataSource = new HikariDataSource(config);
+		return dataSource;
+	}
 
-    @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-        sqlSessionFactory.setConfigLocation(applicationContext.getResource("classpath:/mybatis-config.xml"));
-        sqlSessionFactory.setDataSource(dataSource());
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setConfigLocation(applicationContext.getResource("classpath:/mybatis-config.xml"));
+		sqlSessionFactory.setDataSource(dataSource());
 
-        return sqlSessionFactory.getObject();
-    }
+		return sqlSessionFactory.getObject();
+	}
 
-    @Bean
-    public DataSourceTransactionManager transactionManager() {
-        DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource());
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource());
 
-        return manager;
-    }
+		return manager;
+	}
 
 }
