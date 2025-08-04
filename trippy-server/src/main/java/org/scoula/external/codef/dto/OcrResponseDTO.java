95fa49f5b43533699180afeed9891e9700b91a35
bangbang444 <kim1228com@naver.com>
2025-08-04 14:00:02 +0900

    feat: Ocr 응답 DTO 작성

---
+++ trippy-server/src/main/java/org/scoula/external/codef/dto/OcrResponseDTO.java
@@ -0,0 +1,16 @@
+package org.scoula.external.codef.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OcrResponseDTO {
+    private String resIdCardType;
+    private String resUserName;
+    private String resIdCard;
+    private String resIssueDate;
+    private String resUserIdentity;
+}
