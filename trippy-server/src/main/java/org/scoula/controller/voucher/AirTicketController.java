ac876168c7401fca3a703c097efc96ba7f6e9deb
hellozo0 <ahju0011@gmail.com>
2025-08-04 15:24:28 +0900

    feat : 항공권 전체 조회 Controller

---
+++ trippy-server/src/main/java/org/scoula/controller/voucher/AirTicketController.java
@@ -0,0 +1,45 @@
+package org.scoula.controller.voucher;
+
+import java.util.List;
+
+import org.scoula.common.dto.ErrorResponse;
+import org.scoula.common.dto.SuccessResponse;
+import org.scoula.common.exception.enums.SuccessCode;
+import org.scoula.controller.voucher.dto.response.AirTicketDto;
+import org.scoula.service.voucher.AirTicketService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+
+@Api(tags = "Air Ticket")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/air-ticket")
+public class AirTicketController {
+	private final AirTicketService airTicketService;
+
+	/**
+	 * 추후에 @ApiIgnore @RequestParam Long userId와 같은 형식으로 수정해야 합니다.
+	 * 그리고 헤더에 담긴 JWT 토큰으로 유저 정보를 받아와서 알아서 userId 파라미터로 넣어줍니다...
+	 * 8/5(화)에 JWT 작업 예정
+	 * **/
+	@ApiOperation(value = "[JWT] 항공권 전제 조회", notes = "항공권 전체 조회 API")
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "항공권 전체조회 성공", response = SuccessResponse.class),
+		@ApiResponse(code = 404, message = "해당 유저가 존재하지 않습니다.", response = ErrorResponse.class)
+	})
+	@PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+	public SuccessResponse<List<AirTicketDto>> getAirTicket(@RequestParam Long userId) {
+
+		return SuccessResponse.success(SuccessCode.FIND_AIR_TICKET_SUCCESS, airTicketService.getAirTicket(userId));
+	}
+
+}
