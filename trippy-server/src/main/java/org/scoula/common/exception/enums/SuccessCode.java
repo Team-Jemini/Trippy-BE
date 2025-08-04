61954cf6b4689e8db2d30945cc1ed76627f4243c
hellozo0 <ahju0011@gmail.com>
2025-08-04 15:27:05 +0900

    feat : 성공  Code 추가

--- trippy-server/src/main/java/org/scoula/common/exception/enums/SuccessCode.java
+++ trippy-server/src/main/java/org/scoula/common/exception/enums/SuccessCode.java
@@ -11,6 +11,7 @@ public enum SuccessCode {
 	CREATE_GROUP_ACCOUNT_SUCCESS(HttpStatus.OK, "모임 계좌가 성공적으로 생성되었습니다."),
 	CREATE_INVITE_TOKEN_SUCCESS(HttpStatus.OK, "초대 링크가 성공적으로 생성 되었습니다."),
 	PARSE_INVITE_TOKEN_SUCCESS(HttpStatus.OK, "초대 토큰을 성공적으로 파싱했습니다."),
+	FIND_AIR_TICKET_SUCCESS(HttpStatus.OK, "항공권 전체조회 성공"),
 	;
 
 	private final HttpStatus httpStatus;
