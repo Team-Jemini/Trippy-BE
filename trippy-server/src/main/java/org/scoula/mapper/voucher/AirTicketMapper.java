6988f694c72c51f2f2cefb98e1056d407e278e89
hellozo0 <ahju0011@gmail.com>
2025-08-04 15:28:56 +0900

    feat : 항공권 전체 조회하기 내림차순 조회

--- trippy-server/src/main/java/org/scoula/mapper/voucher/AirTicketMapper.java
+++ trippy-server/src/main/java/org/scoula/mapper/voucher/AirTicketMapper.java
@@ -1,7 +1,12 @@
 package org.scoula.mapper.voucher;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.scoula.domain.voucher.AirTicketVO;
 
 @Mapper
 public interface AirTicketMapper {
+
+	List<AirTicketVO> findAllByUserIdOrderByDepartureDateDesc(Long userId);
 }
