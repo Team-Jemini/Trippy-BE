f4f75beeac053ed33dd8c5c3a8f1ebb99b43a874
Jaejeong Lee <leejae938@gmail.com>
2025-08-05 11:32:39 +0900

    [chore] 파일 위치 변경

--- trippy-server/src/main/java/org/scoula/external/exchangeApi/dto/ExchangeRateApiDTO.java
+++ trippy-server/src/main/java/org/scoula/external/exchangeApi/dto/ExchangeRateApiDTO.java
@@ -1,4 +1,4 @@
-package org.scoula.controller.exchange.dto;
+package org.scoula.external.exchangeApi.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
