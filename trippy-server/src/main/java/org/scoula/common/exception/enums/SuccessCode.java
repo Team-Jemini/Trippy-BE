8f17015f0de8d1671537f787a832cee60edb14d0
hsw <hsw8245@gmail.com>
2025-08-05 10:02:09 +0900

    fix : 깃 충돌 해결

--- trippy-server/src/main/java/org/scoula/common/exception/enums/SuccessCode.java
+++ trippy-server/src/main/java/org/scoula/common/exception/enums/SuccessCode.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 public enum SuccessCode {
 
 	FIND_AIR_TICKET_SUCCESS(HttpStatus.OK, "항공권 전체조회 성공"),
-	REFRESH_SUCCESS(HttpStatus.OK, "토큰 갱신 성공입니다.");
 	REFRESH_SUCCESS(HttpStatus.OK, "토큰 갱신 성공입니다."),
 	FIND_TRAVEL_LOG_SUCCESS(HttpStatus.OK, "여행 로그 조회 성공");
 
