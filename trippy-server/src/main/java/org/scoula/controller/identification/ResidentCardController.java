b4ef11cbbe4ea851ef247b1b75e15a90541880ce
bangbang444 <kim1228com@naver.com>
2025-08-05 17:15:20 +0900

    feat: 주민등록증 등록 컨트롤러 작성

--- trippy-server/src/main/java/org/scoula/controller/identification/ResidentCardController.java
+++ trippy-server/src/main/java/org/scoula/controller/identification/ResidentCardController.java
@@ -4,15 +4,12 @@ import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.scoula.common.dto.SuccessResponse;
 import org.scoula.common.exception.enums.SuccessCode;
-import org.scoula.controller.identification.dto.ResidentCardDTO;
+import org.scoula.controller.identification.dto.req.ResidentCardReq;
+import org.scoula.controller.identification.dto.res.ResidentCardDTO;
 import org.scoula.external.codef.identification.OcrService;
-import org.scoula.controller.identification.dto.ResidentCardOcrDTO;
+import org.scoula.controller.identification.dto.res.ResidentCardOcrDTO;
 import org.scoula.service.identification.ResidentCardService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -50,5 +47,21 @@ public class ResidentCardController {
     public SuccessResponse<ResidentCardDTO> getResidentCardInfo(@RequestParam("userId") Long userId){
 
         return SuccessResponse.success(SuccessCode.RESIDENT_CARD_SUCCESS, residentCardService.getResidentCardInfo(userId));
+    }
+
+    @ApiOperation(value = "[JWT] 주민등록증 추가", notes = "주민등록증 추가하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "주민등록증 추가에 성공했습니다.", response = SuccessResponse.class),
+            @ApiResponse(code = 400, message = "잘못된 요청입니다."),
+            @ApiResponse(code = 500, message = "서버에서 오류가 발생했습니다.")
+    })
+    @PostMapping("/residentCard")
+    public SuccessResponse<Integer> addResidentCardInfo(
+            @RequestHeader("X-USER-ID") Long userId,
+            @RequestBody ResidentCardReq residentCardReq){
+
+        Integer affectedRows = residentCardService.addResidentCardInfo(userId, residentCardReq);
+
+        return SuccessResponse.success(SuccessCode.RESIDENT_CARD_ADD_SUCCESS, affectedRows);
     }
 }
