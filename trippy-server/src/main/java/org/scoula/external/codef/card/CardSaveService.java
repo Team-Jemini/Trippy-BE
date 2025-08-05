fe347c5434bbe9f08fabfae885844b829ff4aa89
juwon1105 <amy1105a@naver.com>
2025-08-05 15:19:38 +0900

    fix : @RequireArgsConstructor 사용

--- trippy-server/src/main/java/org/scoula/external/codef/card/CardSaveService.java
+++ trippy-server/src/main/java/org/scoula/external/codef/card/CardSaveService.java
@@ -4,16 +4,15 @@ import org.scoula.domain.card.CardVO;
 import org.scoula.mapper.card.CardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import org.scoula.domain.card.CardVO;
+import org.scoula.mapper.card.CardMapper;
 
 @Service
+@RequiredArgsConstructor
 public class CardSaveService {
 
 	private final CardMapper cardMapper;
-
-	@Autowired
-	public CardSaveService(CardMapper cardMapper) {
-		this.cardMapper = cardMapper;
-	}
 
 	public void saveCard(CardVO cardVO) {
 		cardVO.setUserId(1L); // 예시
