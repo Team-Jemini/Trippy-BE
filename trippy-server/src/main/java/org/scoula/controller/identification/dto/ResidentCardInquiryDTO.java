99eba43e101a66f506b005cdf250e4aec3c632eb
bangbang444 <kim1228com@naver.com>
2025-08-05 09:52:36 +0900

    chore: 불필요 import 제거

--- trippy-server/src/main/java/org/scoula/controller/identification/dto/ResidentCardInquiryDTO.java
+++ trippy-server/src/main/java/org/scoula/controller/identification/dto/ResidentCardInquiryDTO.java
@@ -1,8 +1,5 @@
 package org.scoula.controller.identification.dto;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
 public record ResidentCardInquiryDTO(
         String resUserName,
         String resIssueDate,
