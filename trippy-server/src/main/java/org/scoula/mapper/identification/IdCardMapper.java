ebe080c1cee51ab3cb0d39fd59afedfae320550a
bangbang444 <kim1228com@naver.com>
2025-08-05 14:25:13 +0900

    feat: Mapper 선언

--- trippy-server/src/main/java/org/scoula/mapper/identification/IdCardMapper.java
+++ trippy-server/src/main/java/org/scoula/mapper/identification/IdCardMapper.java
@@ -3,6 +3,7 @@ package org.scoula.mapper.identification;
 import org.apache.ibatis.annotations.Mapper;
 import org.scoula.domain.identification.IdCardVO;
 
+
 @Mapper
 public interface IdCardMapper {
     IdCardVO getResidentCardInfo(Long userId);
