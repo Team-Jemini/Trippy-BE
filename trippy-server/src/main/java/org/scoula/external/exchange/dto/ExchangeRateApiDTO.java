21c7549405e83d48403ef3895776aa7511a1cb9c
hellozo0 <ahju0011@gmail.com>
2025-08-06 10:17:54 +0900

    style : 코드 스타일 수정

--- trippy-server/src/main/java/org/scoula/external/exchange/dto/ExchangeRateApiDTO.java
+++ trippy-server/src/main/java/org/scoula/external/exchange/dto/ExchangeRateApiDTO.java
@@ -2,43 +2,44 @@ package org.scoula.external.exchange.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import org.scoula.domain.exchange.ExchangeRateVO;
 
 import java.time.LocalDateTime;
 
 @JsonIgnoreProperties(ignoreUnknown = true) // 필요없는 json 데이터 제거
-public record ExchangeRateApiDTO (
-    Long exchangeRateId,
+public record ExchangeRateApiDTO(
+	Long exchangeRateId,
 
-    @JsonProperty("cur_unit")
-    String currencyCode,
+	@JsonProperty("cur_unit")
+	String currencyCode,
 
-    @JsonProperty("cur_nm")
-    String currencyName,
+	@JsonProperty("cur_nm")
+	String currencyName,
 
-    @JsonProperty("deal_bas_r")
-    String baseExchangeRate, // String -> double
+	@JsonProperty("deal_bas_r")
+	String baseExchangeRate, // String -> double
 
-    Double rateBuy,
-    Double rateSell,
-    LocalDateTime exchangeRateDate
+	Double rateBuy,
+	Double rateSell,
+	LocalDateTime exchangeRateDate
 ) {
 
-    public double getExchangeRateDouble(String baseExchangeRate) {
-        String withoutComma = baseExchangeRate.replace(",", "");
-        return Double.parseDouble(withoutComma);
-    }
+	public double getExchangeRateDouble(String baseExchangeRate) {
+		String withoutComma = baseExchangeRate.replace(",", "");
+		return Double.parseDouble(withoutComma);
+	}
 
-    public ExchangeRateVO toExchangeRateVO(String baseExchangeRate) {
-        return ExchangeRateVO.builder()
-            .exchangeRateId(exchangeRateId)
-            .currencyCode(currencyCode)
-            .currencyName(currencyName)
-            .baseExchangeRate(getExchangeRateDouble(baseExchangeRate))
-            .rateBuy(getExchangeRateDouble(baseExchangeRate))
-            .rateSell(getExchangeRateDouble(baseExchangeRate))
-            .exchangeRateDate(exchangeRateDate)
-            .build();
-    }
+	public ExchangeRateVO toExchangeRateVO(String baseExchangeRate) {
+		return ExchangeRateVO.builder()
+			.exchangeRateId(exchangeRateId)
+			.currencyCode(currencyCode)
+			.currencyName(currencyName)
+			.baseExchangeRate(getExchangeRateDouble(baseExchangeRate))
+			.rateBuy(getExchangeRateDouble(baseExchangeRate))
+			.rateSell(getExchangeRateDouble(baseExchangeRate))
+			.exchangeRateDate(exchangeRateDate)
+			.build();
+	}
 
 }
