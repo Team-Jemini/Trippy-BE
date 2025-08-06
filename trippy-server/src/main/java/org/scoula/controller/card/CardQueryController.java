eb8ceb081e768b38e441f2376404f3b5c75e1769
juwon1105 <amy1105a@naver.com>
2025-08-06 15:34:18 +0900

    fix : swagger 수정

--- trippy-server/src/main/java/org/scoula/controller/card/CardQueryController.java
+++ trippy-server/src/main/java/org/scoula/controller/card/CardQueryController.java
@@ -8,6 +8,10 @@ import org.scoula.service.card.CardQueryService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.scoula.common.dto.SuccessResponse;
+import org.scoula.common.dto.ErrorResponse;
+import org.scoula.common.exception.enums.SuccessCode;
+
 
 @RestController
 @RequestMapping("/cards")
@@ -16,18 +20,31 @@ import java.util.List;
 public class CardQueryController {
 
 	private final CardQueryService cardQueryService;
-
-	@ApiOperation(value = "카드 요약 목록 조회", notes = "/payment 화면에 필요한 정보 조회")
+	@ApiOperation(value = "카드 요약 목록 조회", notes = "/payment 화면에 필요한 카드 요약 정보 조회 API")
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "카드 요약 목록 조회 성공", response = SuccessResponse.class),
+		@ApiResponse(code = 404, message = "해당 유저가 존재하지 않습니다.", response = ErrorResponse.class)
+	})
 	@GetMapping("/summary")
-	public List<CardSummaryResponseDTO> getCardSummaries(
-		@ApiParam(value = "유저 ID", required = true) @RequestParam Long userId) {
-		return cardQueryService.getCardSummaries(userId);
+	public SuccessResponse<List<CardSummaryResponseDTO>> getCardSummaries(
+		@ApiParam(value = "유저 ID", required = true, example = "101")
+		@RequestParam Long userId
+	) {
+		List<CardSummaryResponseDTO> summaries = cardQueryService.getCardSummaries(userId);
+		return SuccessResponse.success(SuccessCode.FIND_CODEF_CARD_QUERY_SUCCESS, summaries);
 	}
 
-	@ApiOperation(value = "카드 상세 목록 조회", notes = "/payment/settings 화면에 필요한 정보 조회")
+	@ApiOperation(value = "카드 상세 목록 조회", notes = "/payment/settings 화면에 필요한 카드 상세 정보 조회 API")
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "카드 상세 목록 조회 성공", response = SuccessResponse.class),
+		@ApiResponse(code = 404, message = "해당 유저가 존재하지 않습니다.", response = ErrorResponse.class)
+	})
 	@GetMapping("/detail")
-	public List<CardDetailResponseDTO> getCardDetails(
-		@ApiParam(value = "유저 ID", required = true) @RequestParam Long userId) {
-		return cardQueryService.getCardDetails(userId);
+	public SuccessResponse<List<CardDetailResponseDTO>> getCardDetails(
+		@ApiParam(value = "유저 ID", required = true, example = "101")
+		@RequestParam Long userId
+	) {
+		List<CardDetailResponseDTO> details = cardQueryService.getCardDetails(userId);
+		return SuccessResponse.success(SuccessCode.FIND_CODEF_CARD_QUERY_SUCCESS, details);
 	}
 }
