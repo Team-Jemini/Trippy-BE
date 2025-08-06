a832a01b6ed37ccc3abb7f8420fe2a724d452e6c
bangbang444 <kim1228com@naver.com>
2025-08-06 09:46:19 +0900

    feat: 여권번호 필드 추가

--- trippy-server/src/main/java/org/scoula/controller/identification/dto/req/PassportReq.java
+++ trippy-server/src/main/java/org/scoula/controller/identification/dto/req/PassportReq.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record PassportReq(
+        String passportNumber,
         String nameKr,
         String nameEn,
         LocalDate birthDate,
