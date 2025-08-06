066a956e11bfdd21d3e9792abf7a1d3c4a6abd48
juwon1105 <amy1105a@naver.com>
2025-08-06 14:35:52 +0900

    fix : record로 수정

--- trippy-server/src/main/java/org/scoula/external/codef/card/CardUserInfo.java
+++ trippy-server/src/main/java/org/scoula/external/codef/card/CardUserInfo.java
@@ -1,10 +1,8 @@
 package org.scoula.external.codef.card;
 
-public class CardUserInfo {
-	private String loginId;
-	private String password;
-	private String birthDate;
-	private String organization;
-
-	// Getter, Setter
-}
+public record CardUserInfo (
+	String loginId,
+	String password,
+	String birthDate,
+	String organization
+){}
