2d84d72697babc70b25ec087e0e921bcbb43307a
Jaejeong Lee <leejae938@gmail.com>
2025-08-06 14:08:06 +0900

    [chore] 불필요한 함수 삭제

--- trippy-server/src/main/java/org/scoula/controller/exchange/dto/ExchangeBalanceResponse.java
+++
@@ -1,7 +0,0 @@
-package org.scoula.controller.exchange.dto;
-
-public class ExchangeBalanceResponse extends ExchRateAndBalanceResponse {
-    public ExchangeBalanceResponse(String currencyCode, Double rate, Long krwBalance, Double foreignBalance) {
-
-    }
-}
