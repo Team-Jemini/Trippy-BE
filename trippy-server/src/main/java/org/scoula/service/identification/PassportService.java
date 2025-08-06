7db71131521bf9459a79a2e6feaffe8dc53e69b4
bangbang444 <kim1228com@naver.com>
2025-08-06 09:22:29 +0900

    feat: 여권 등록 서비스 작성

--- trippy-server/src/main/java/org/scoula/service/identification/PassportService.java
+++ trippy-server/src/main/java/org/scoula/service/identification/PassportService.java
@@ -2,6 +2,8 @@ package org.scoula.service.identification;
 
 import lombok.RequiredArgsConstructor;
 import org.scoula.controller.identification.dto.PassportDTO;
+import org.scoula.controller.identification.dto.req.PassportReq;
+import org.scoula.domain.identification.PassportVO;
 import org.scoula.mapper.identification.PassportMapper;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,9 @@ public class PassportService {
 
     public PassportDTO getPassport(Long userId){
         return PassportDTO.from(passportMapper.findByUserId(userId));
+    }
+
+    public int addPassport(Long userId, PassportReq req){
+        return passportMapper.save(PassportVO.from(userId, req));
     }
 }
