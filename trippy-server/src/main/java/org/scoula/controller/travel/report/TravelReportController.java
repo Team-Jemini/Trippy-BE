06881ce2b61e2e8a04dcbf038c315178de6fcf7c
hsw <hsw8245@gmail.com>
2025-08-07 16:22:50 +0900

    feat : 여행리포트 조회 API

--- trippy-server/src/main/java/org/scoula/controller/travel/report/TravelReportController.java
+++ trippy-server/src/main/java/org/scoula/controller/travel/report/TravelReportController.java
@@ -1,4 +1,35 @@
 package org.scoula.controller.travel.report;
 
+import lombok.RequiredArgsConstructor;
+import org.scoula.common.dto.SuccessResponse;
+import org.scoula.common.exception.enums.SuccessCode;
+import org.scoula.controller.travel.report.dto.res.TravelReportDTO;
+import org.scoula.service.travel.TravelReportService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import io.swagger.annotations.*;
+
+@Api(tags = "Travel")
+@RestController
+@RequestMapping("/travel-report")
+@RequiredArgsConstructor
 public class TravelReportController {
+    private final TravelReportService travelReportService;
+
+    @ApiOperation(value = "[JWT] 여행 리포트 조회", notes = "여행 ID로 소비 리포트를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "여행 소비 리포트 조회 성공"),
+            @ApiResponse(code = 404, message = "해당 여행이 존재하지 않습니다.")
+    })
+    @GetMapping("/{travelId}/travel-report")
+    public SuccessResponse<TravelReportDTO> getTravelReport(
+            @ApiParam(value = "여행 ID", required = true, example = "5")
+            @PathVariable Long travelId
+    ) {
+        return SuccessResponse.success(SuccessCode.FIND_TRAVEL_REPORT_SUCCESS, travelReportService.getTravelReport(travelId));
+    }
 }
