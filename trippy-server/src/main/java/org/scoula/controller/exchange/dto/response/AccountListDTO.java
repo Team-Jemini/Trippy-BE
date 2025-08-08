98a3530754a6896fd33366ff82dfba93beccedeb
hellozo0 <ahju0011@gmail.com>
2025-08-08 23:42:38 +0900

    style : 코드 스타일 포매팅

--- trippy-server/src/main/java/org/scoula/controller/exchange/dto/response/AccountListDTO.java
+++ trippy-server/src/main/java/org/scoula/controller/exchange/dto/response/AccountListDTO.java
@@ -1,20 +1,22 @@
 package org.scoula.controller.exchange.dto.response;
 
 import org.scoula.domain.account.DeletedStatus;
+import org.scoula.domain.exchange.AccountListVO;
 
 public record AccountListDTO(
 	String accountId,
 	String accountName,
 	Long balance,
 	String accountForeign,
-	DeletedStatus isDeleted) {
-	public static AccountListDTO from(
-		String accountId,
-		String accountName,
-		Long balance,
-		String accountForeign,
-		DeletedStatus isDeleted
-	) {
-		return new AccountListDTO(accountId, accountName, balance, accountForeign, isDeleted);
+	DeletedStatus isDeleted
+) {
+	public static AccountListDTO from(AccountListVO vo) {
+		return new AccountListDTO(
+			vo.getAccountId(),
+			vo.getAccountName(),
+			vo.getBalance(),
+			vo.getAccountCurrency(),
+			vo.getIsDeleted()
+		);
 	}
 }
