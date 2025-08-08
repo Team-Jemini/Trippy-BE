b05a7735bd2ddd9c5c11b94ab2d01475967fbb08
hellozo0 <ahju0011@gmail.com>
2025-08-08 10:46:41 +0900

    feat : 스웨거 헤더 커스텀

--- trippy-server/src/main/java/org/scoula/config/ServletConfig.java
+++ trippy-server/src/main/java/org/scoula/config/ServletConfig.java
@@ -1,17 +1,27 @@
 package org.scoula.config;
 
+import java.util.List;
+
+import org.scoula.config.swagger.MultipartJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 @EnableWebMvc
 @ComponentScan(basePackages = {
     "org.scoula.exception",
-    "org.scoula.controller"
+    "org.scoula.controller",
+    "org.scoula.config.swagger"
 })
 public class ServletConfig implements WebMvcConfigurer {
 
@@ -35,6 +45,18 @@ public class ServletConfig implements WebMvcConfigurer {
             .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/v2/api-docs")
             .addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        // 커스텀 멀티파트 컨버터 추가
+        converters.add(new MultipartJackson2HttpMessageConverter(mapper));
+        // 기본 Jackson 컨버터도 추가
+        converters.add(new MappingJackson2HttpMessageConverter(mapper));
     }
 
 }
--- trippy-server/src/main/java/org/scoula/config/swagger/SwaggerConfig.java
+++ trippy-server/src/main/java/org/scoula/config/swagger/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package org.scoula.config.swagger;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -19,7 +25,7 @@ public class SwaggerConfig {
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 			.title("Trippy API")
-			.description("여행용 전자지갑앱 Trippy Swagegr입니다.")
+			.description("여행용 전자지갑앱 Trippy Swagger입니다.")
 			.version("1.0.0")
 			.build();
 	}
@@ -31,6 +37,25 @@ public class SwaggerConfig {
 			.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
 			.paths(PathSelectors.any())
 			.build()
-			.apiInfo(apiInfo());
+			.apiInfo(apiInfo())
+			.securitySchemes(List.of(apiKey()))             // ✅ Authorize 버튼 생성
+			.securityContexts(List.of(securityContext()));  // ✅ 모든 API에 자동 적용
+	}
+
+	private ApiKey apiKey() {
+		// name = UI 표시 이름, keyName = 헤더명, passAs = 전달 위치
+		return new ApiKey("JWT", "Authorization", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+			.securityReferences(defaultAuth())
+			.forPaths(PathSelectors.any())
+			.build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		return List.of(new SecurityReference("JWT", new AuthorizationScope[]{authorizationScope}));
 	}
 }
