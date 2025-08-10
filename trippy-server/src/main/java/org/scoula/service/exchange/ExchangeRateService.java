238514cd1a60e23037b2c9d45a74edc2643a29ce
hellozo0 <ahju0011@gmail.com>
2025-08-10 12:31:09 +0900

    fix : 계좌 목록 조회 수정

--- trippy-server/src/main/java/org/scoula/service/exchange/ExchangeRateService.java
+++ trippy-server/src/main/java/org/scoula/service/exchange/ExchangeRateService.java
@@ -12,6 +12,8 @@ import org.scoula.service.user.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,13 +42,15 @@ public class ExchangeRateService {
 	public List<AccountListDTO> getAccountList(Long userId) {
 		userService.validateUserExists(userId);
 
-		var list = Optional.ofNullable(exchangeRateMapper.getAccountList(userId))
-			.orElseGet(List::of);
+		List<AccountListVO> accountList = exchangeRateMapper.getAccountList(userId);
 
-		return list.stream()
+		return Optional.ofNullable(accountList)
+			.orElse(Collections.emptyList())
+			.stream()
 			.map(AccountListDTO::from)
 			.toList();
 	}
+
 
 	/***
 	 * 오늘의 환율, 해당 계좌의 KRW 잔액, 지정 외화의 잔액을 한 번에 반환.
@@ -56,17 +60,16 @@ public class ExchangeRateService {
 	 * @return
 	 */
 	public ExchangeBalanceDTO getRatesAndBalance(Long userId, String currencyCode, String accountId) {
-		userService.validateUserExists(userId);
-		
-		ExchangeRateVO exchangeRateVO = exchangeRateMapper.findTodayRateByCurrencyCode(currencyCode);
+		ExchangeRateVO  exchangeRateVO = exchangeRateMapper.findTodayRateByCurrencyCode(currencyCode);
 		Double rate = exchangeRateVO.getBaseExchangeRate();
 
 		AccountListVO accountListVo = exchangeRateMapper.findKrwBalanceByAccountId(accountId);
 		Long krwBalance = accountListVo.getBalance();
 
 		Double foreignBalance = exchangeRateMapper.findForeignBalanceByAccountIdAndCurrency(userId, currencyCode);
+		ExchangeBalanceDTO exchangeBalanceDTO = ExchangeBalanceDTO.from(currencyCode, rate, krwBalance, foreignBalance);
 
-		return ExchangeBalanceDTO.from(currencyCode, rate, krwBalance, foreignBalance);
+		return exchangeBalanceDTO;
 	}
 
 	@Transactional
