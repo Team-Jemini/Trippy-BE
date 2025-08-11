3c341603cd01b3d088702acbe98ef33d3e5af91b
hellozo0 <ahju0011@gmail.com>
2025-08-11 16:07:08 +0900

    docs : 엑세스 토큰 리프레시 dto

--- trippy-server/src/main/java/org/scoula/controller/user/dto/request/TokenRequestDto.java
+++ trippy-server/src/main/java/org/scoula/controller/user/dto/request/TokenRequestDto.java
@@ -1,6 +1,12 @@
 package org.scoula.controller.user.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "액세스 토큰 리프레시 DTO")
 public record TokenRequestDto(
+	@ApiModelProperty(value = "엑세스 토큰", example = "eyJ0e~")
 	String accessToken,
+	@ApiModelProperty(value = "리프레시 토큰", example = "eyJ0e~")
 	String refreshToken) {
 }
