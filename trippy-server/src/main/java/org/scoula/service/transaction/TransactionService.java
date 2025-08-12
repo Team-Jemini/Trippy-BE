bed455dc19e3634bcf8753f0f7e9496f56f56ad2
hellozo0 <ahju0011@gmail.com>
2025-08-12 15:45:46 +0900

    feat :여행기간동안 전체 지출 조회 Service

---
+++ trippy-server/src/main/java/org/scoula/service/transaction/TransactionService.java
@@ -0,0 +1,23 @@
+package org.scoula.service.transaction;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.scoula.domain.transaction.TransactionVO;
+import org.scoula.mapper.transaction.TransactionMapper;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+
+	private final TransactionMapper transactionMapper;
+
+	public List<TransactionVO> getUserExpenseTransactions(Long userId, String accountId, LocalDateTime startDate, LocalDateTime endDate){
+		return transactionMapper.findExpenseTransactionsByAccountId(userId, accountId, startDate, endDate);
+	}
+}
